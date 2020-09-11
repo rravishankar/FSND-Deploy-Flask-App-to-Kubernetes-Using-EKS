57838697a86918b8efe3378a8efbd272b61fca5b
rravishankar <rravi108@gmail.com>
2020-09-11 17:42:07 +0530

    Added comment for a TBD using environment variables for managing secret data and also a built-in Kubernetes mechanism

--- test_main.py
+++ test_main.py
@@ -8,6 +8,7 @@ import pytest
 import main
 
 SECRET = 'TestSecret'
+#TBD Consider using some environment variables management strategy while dealing with sensitive data like tokens and secrets. Since you are working with k8s you can rely on the built-in mechanism: https://kubernetes.io/docs/concepts/configuration/secret/
 TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NjEzMDY3OTAsIm5iZiI6MTU2MDA5NzE5MCwiZW1haWwiOiJ3b2xmQHRoZWRvb3IuY29tIn0.IpM4VMnqIgOoQeJxUbLT-cRcAjK41jronkVrqRLFmmk'
 EMAIL = 'wolf@thedoor.com'
 PASSWORD = 'huff-puff'
